83e47626c3392ac10973d1913e2924ed149021e4
hasinthad <hasinthad@codegen.net>
2025-04-18 17:26:48 +0530

    service discovery working with eureka. order service fails

--- order-service/src/main/java/com/floo/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/floo/order_service/controller/OrderController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/order")
+@RequestMapping("/api/v1/order")
 public class OrderController {
 
     @GetMapping("/test")
