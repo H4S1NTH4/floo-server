df66ba3f78afc2d4c503590980eefcf3ba72c1ad
hasinthad <hasinthad@codegen.net>
2025-05-15 15:03:08 +0530

    deliver service

--- delivery-service/src/main/java/com/floo/delivery_service/repository/DriverRepository.java
+++ delivery-service/src/main/java/com/floo/delivery_service/repository/DriverRepository.java
@@ -2,8 +2,11 @@ package com.floo.delivery_service.repository;
 import com.floo.delivery_service.entity.Driver;
 import com.floo.delivery_service.entity.DriverStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface DriverRepository extends MongoRepository<Driver, String> {
     List<Driver> findByStatusAndAvailable(DriverStatus status, Boolean available);
 }
